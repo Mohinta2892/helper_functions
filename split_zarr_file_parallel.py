41ad2172cfa6ff58fb2fcbeb920e2c279741dc61
Samia Mohinta <44754434+Mohinta2892@users.noreply.github.com>
2023-03-30 16:56:57 +0100

    with comments

--- split_zarr_file_parallel.py
+++ split_zarr_file_parallel.py
@@ -1,3 +1,21 @@
+"""
+Program desc:
+Splits an n5 directory source into regions of interest parallely.
+Checks if the roi actually contains neural data or is completely background (Resin). Discards solely resin rois from
+being saved to disk.
+
+Input n5 : An n5 directory source that is read in as zarr N5Filestore
+Input mask: A downsampled mask which distinguishes foreground (necessary in roi) and backhground (to be discarded)
+Output: Regions of interest of predefined size in the code
+
+Parallel processing: Uses Python multiprocessing Pool
+Warning: Can be slow, future rewrite with daisy/dask
+
+Author: Samia Mohinta
+Mask generated and implementation logic - Pedro Gómez-Gálvez
+
+"""
+
 import numpy as np
 import zarr
 from multiprocessing import Pool
@@ -12,9 +30,10 @@ Notes:
 Matrix shape of octo scale 0 in zyx:(34745, 21471, 20486)
 Matrix shape of octo scale 6 in zyx:(542, 335, 320)
 Ratio changes = 542/34745 = 0.02; 0.02; 0.02
+
+Example:
 Patch size per 1568^3 roi is 31^3 in the scale 0 mask
 mask_patch = mask[:31, :31, :31]
-roi = 
 """
 
 in_store = N5FSStore('/media/samia/DATA/mounts/zstore1/FIBSEM_L1120_FullCNS_8x8x8nm')
@@ -28,11 +47,17 @@ scale_0_mask = tf.imread('/media/samia/DATA/ark/code-experiments/scale_0_mask/ot
 # Define the shape of the chunks to be saved
 chunk_shape = (256, 256, 256)
 
-# do better
+# calculate the scale factor. used to transform the scale 0 indices to scale 6 level
 factors_zyx = [542 / 34745, 335 / 21471, 320 / 20486]
 
 
+# calculate transformed coordinates of scale 0 ndarray as per scale 6 shape
 def multiply_slices(slices, factor=factors_zyx):
+    """
+    :param slices: tuple of slice objects pointing to the region of interest (ROI) in the larger ndarray
+    :param factor: list of floats to be used to scale the indices of the larger ndarray ROI to be mask shape
+    :return: tuple of slice objects that have been scaled to the scale 6 mask
+    """
     return tuple(slice(int(s.start * factor[0]) if s.start is not None else None,
                        int(s.stop * factor[1]) if s.stop is not None else None,
                        int(s.step * factor[2]) if s.step is not None else None)
@@ -40,6 +65,11 @@ def multiply_slices(slices, factor=factors_zyx):
 
 
 def check_for_actual_data(slices):
+    """
+
+    :param slices:
+    :return:
+    """
     mask_patch = scale_0_mask[slices]
 
     mask_contains_foreground = np.nonzero(mask_patch)
@@ -59,7 +89,7 @@ def read_roi_and_save_chunk(filename, region_of_interest):
     slices = multiply_slices(region_of_interest, factors_zyx)
     print(region_of_interest, slices)
     if check_for_actual_data(slices):
-        roi_data = roi_data.rechunk(chunks='auto')
+        roi_data = roi_data.rechunk(chunks='auto')  # replace `auto` with chunk_shape when needed
         # Open the zarr file for writing and write the data to the file
         roi_data.to_zarr(filename, component='volumes/raw')
         with zarr.open(filename, mode='a') as z:
@@ -68,6 +98,8 @@ def read_roi_and_save_chunk(filename, region_of_interest):
     else:
         # skip that roi_data from saving on disk
         print(f"Skipped this: {filename}")
+
+    # update the progress
     pbar.update(1)
 
 
