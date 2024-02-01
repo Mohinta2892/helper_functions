edf6b8b0cb269126253f9d16f297666555531010
Samia Mohinta <44754434+Mohinta2892@users.noreply.github.com>
2024-02-01 13:23:55 +0000

    cast labels to uint64

--- tiff_to_zarr.py
+++ tiff_to_zarr.py
@@ -1,8 +1,14 @@
 import zarr
 import tifffile
+import numpy as np
+
 f = tifffile.imread('path/to/tiff')
 outfile = 'path/to/zarr/filename.zarr'
 ds = 'volumes/labels/neuron_ids' # example dataset
+
+# if you are dealing with tifs that are saved as `labels.tif` (containing labels), this helps them to load them into napari later
+# f = f.astype(np.uint64)
+
 o = zarr.open(outfile, mode='w')
 o[ds] = f
 o[ds].attrs['resolution'] = (8,8,8)
