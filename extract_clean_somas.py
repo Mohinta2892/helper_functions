06014169880438da9f3701074fb8b339e0c5f57d
Samia Mohinta <44754434+Mohinta2892@users.noreply.github.com>
2024-03-14 13:54:41 +0000

    edit's on Mac

--- extract_clean_somas.py
+++ extract_clean_somas.py
@@ -1,4 +1,5 @@
-#!/home/samia/anaconda3/envs/napari-env/bin/python # delete this or run with python from a conda which has napari installed
+#!/Users/cardonagroup/miniconda3/envs/napari-env/bin/python
+
 """
 NB: Ensure you have a good processor and enough RAM because we use `NAPARI`, which loads everything into RAM.
 This script has not been rigorously tested, hence may still have underlying bugs when tested on a different OS.
@@ -6,16 +7,13 @@ This script has not been rigorously tested, hence may still have underlying bugs
 Installation:
 Napari:
 https://napari.org/stable/tutorials/fundamentals/installation
-pip install scikit-image==0.22.0s
+pip install scikit-image==0.22.0
 pip install zarr==2.16.1
 pip install dask==2023.12.1
 pip install numpy==1.26.0
 pip install matplotlib==3.8.0
 pip install scipy==1.11.3
 pip install tqdm==4.66.1
-pip install jupyterlab
-pip install notebook
-python -m ipykernel install --user --name=napari-env # run this to add kernel to jupyter which is used generate the bboxes
 
 Functionality:
 - This script loads a zarr containing raw, neuron labels and mitochondria and visualises them in napari.
@@ -25,6 +23,12 @@ Click to choose the colour and press `a` after every choice to add them to a lis
 Pressing `p` also automatically in-paints and displays the in-painted and intermediate volumes.
 Moreover, it creates an affinity matrix and display it.
 - Press `z` to clear the list.
+
+Run:
+path/to/extract_clean_somas.py -f path/to/zarr -d /path/to/output_folder (this is default in the script)
+Please comment the shebang in Line1 if you do the below:
+$ conda activate napari-env
+$ python path/to/extract_clean_somas.py -f path/to/zarr -d /path/to/output_folder
 
 Test ENV:
 Distributor ID:	Ubuntu
@@ -53,6 +57,7 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 from skimage import morphology, measure
 import time
+from datetime import datetime
 
 viewer = napari.Viewer()
 picked_labels = []
@@ -204,59 +209,84 @@ def pick_in_paint(viewer):
     if len(picked_labels):
         for x in picked_labels:
             cherry_pick_seg[relab == x] = 1  # it is mask
-            print(cherry_pick_seg.dtype)
-            viewer.add_image(cherry_pick_seg, name='cherry_inter')
+            # print(cherry_pick_seg.dtype)
+            # viewer.add_image(cherry_pick_seg, name='cherry_inter')
 
-            # Step1 #
-            thre_mask = remove_holes_2d((cherry_pick_seg * 255).astype(np.uint8))
-            thre_mask = thre_mask.astype(np.uint8)
+            # # Step1 #
+            # thre_mask = remove_holes_2d((cherry_pick_seg * 255).astype(np.uint8))
+            # thre_mask = thre_mask.astype(np.uint8)
 
-            # Step2 #
-            removed_both2d_3d_mask = remove_smalls_2d(thre_mask)
-            removed_both2d_3d_mask = remove_smalls(removed_both2d_3d_mask, min_size=102400)
+            # # Step2 #
+            # removed_both2d_3d_mask = remove_smalls_2d(thre_mask)
+            # removed_both2d_3d_mask = remove_smalls(removed_both2d_3d_mask, min_size=102400)
 
-            # Step3 #
-            result = measure.label(removed_both2d_3d_mask.astype(bool), connectivity=1)
-            result = result.astype(np.uint16)
-            in_paint_seg += result
+            # # Step3 #
+            # result = measure.label(removed_both2d_3d_mask.astype(bool), connectivity=1)
+            # result = result.astype(np.uint16)
+            # in_paint_seg += result
+            # in_paint_seg[relab == 1] = x
+
+            in_paint_seg += cherry_pick_seg
             in_paint_seg[relab == 1] = x
 
-        viewer.add_labels(in_paint_seg, name='in_paint')
-        end = time.time()
-        print(f"Time spent inpainting {end - start}")
+        viewer.add_labels(in_paint_seg, name='picked_labels')
+
         # viewer.add_labels(in_paint_seg, name='cherry_pick_seg')
         # viewer.layers['seg'].visible = False
         # once cherry-picked, generate and display the affinity map
-        aff = seg_to_affgraph(in_paint_seg)
-        viewer.add_image(aff, name='aff', opacity=0.7, blending='additive')
+        # aff = seg_to_affgraph(in_paint_seg)
+        # viewer.add_image(aff, name='aff', opacity=0.7, blending='additive')
 
-        if np.sum(in_paint_seg) > 0:
-            global cher_pick_f
-            cher_pick_f["volumes/labels/neuron_ids"] = in_paint_seg
+        # if np.sum(in_paint_seg) > 0:
+        global cher_pick_f
+        cher_pick_f["volumes/labels/neuron_ids"] = in_paint_seg
+        cher_pick_f["volumes/labels/cherry_pick_seg"] = (cherry_pick_seg*255).astype(np.uint8)
+
             # we set it here since we do not have access to offset and res in `pick_in_paint()`
-            cher_pick_f["volumes/labels/neuron_ids"].attrs["offset"] = (0, 0, 0)  # a hack till we find a better way!!
-            cher_pick_f["volumes/labels/neuron_ids"].attrs["resolution"] = (8, 8, 8)
+        cher_pick_f["volumes/labels/neuron_ids"].attrs["offset"] = (0,0,0)
+        cher_pick_f["volumes/labels/neuron_ids"].attrs["resolution"] = (8,8,8)
+        cher_pick_f["volumes/labels/cherry_pick_seg"].attrs["offset"] = (0,0,0)
+        cher_pick_f["volumes/labels/cherry_pick_seg"].attrs["resolution"] = (8,8,8)
 
-    return None
+
+        
+        end = time.time()
+
+        print(f"Time spent till saving {end - start}")
+        viewer.close()
+        return 1
+
+    return 0
 
 
-def pick_somas(filename, ):
+def pick_somas(filename, out_dir=None):
+    if out_dir is None:
+        out_dir = os.path.dirname(filename)
+
     f = zarr.open(filename, mode='r')
-    global cher_pick_f  # a global cherry-picked and inpainted zarr
-    cher_pick_f = zarr.open(os.path.join(os.path.dirname(filename),
-                                         f"{os.path.basename(filename).split('.zarr')[0]}_picked.zarr"), mode='a')
+    date_time = datetime.now()
+    # format specification
+    format = '%Y-%m-%d %H:%M:%S'
 
-    seg = f["volumes/labels/neuron_ids"][:]
+    # applying strftime() to format the datetime
+    dt_str = date_time.isoformat().strip(" /")
+
+
+    global cher_pick_f  # a global cherry-picked and inpainted zarr
+    cher_pick_f = zarr.open(os.path.join(out_dir,
+                                         f"{os.path.basename(filename).split('.zarr')[0]}_picked_{dt_str}.zarr"), mode='a')
+    crop_roi=(slice(0,f["volumes/raw"].shape[0],None), slice(0,f["volumes/raw"].shape[1],None), slice(0,f["volumes/raw"].shape[2], None))
+    seg = f["volumes/labels/neuron_ids"][crop_roi]
     global relab
     relab, _, _ = skimage.segmentation.relabel_sequential(seg)
     relab = relab.astype(np.uint16)
-    relab_mito, _, _ = skimage.segmentation.relabel_sequential(f["volumes/labels/mito_ids"][:])
+    relab_mito, _, _ = skimage.segmentation.relabel_sequential(f["volumes/labels/mito_ids"][crop_roi])
     relab_mito = relab_mito.astype(np.uint16)
 
     viewer.add_labels(relab, name='seg')
-    viewer.add_image(f["volumes/raw_clahe"][:], name='raw', opacity=0.5, blending='additive')
-    viewer.add_labels(relab_mito, name='mito', opacity=0.5, blending='additive')
-    viewer.layers["mito"].visible = False
+    viewer.add_image(f["volumes/raw_clahe"][crop_roi], name='raw', opacity=0.5, blending='additive')
+    #viewer.add_labels(relab_mito, name='mito', opacity=0.5, blending='additive')
+    #viewer.layers["mito"].visible = False
 
     cher_pick_f["volumes/raw"] = f["volumes/raw_clahe"][:]
     cher_pick_f["volumes/labels/mito_ids"] = relab_mito
@@ -271,13 +301,23 @@ def pick_somas(filename, ):
     cher_pick_f["volumes/labels/mito_ids"].attrs["offset"] = offset
     cher_pick_f["volumes/labels/mito_ids"].attrs["resolution"] = res
 
+    # we set it here since we do not have access to offset and res in `pick_in_paint()`
+    # cher_pick_f["volumes/labels/neuron_ids"].attrs["offset"] = offset
+    # cher_pick_f["volumes/labels/neuron_ids"].attrs["resolution"] = res
+
+
+
     napari.run()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", help="Path/to/zarr")
+    parser.add_argument("-d", default= "/Users/cardonagroup/Documents/hemibrain_soma_bbox/soma_picked", required=False, help="Path/to/output/folder")
 
     args = parser.parse_args()
 
-    pick_somas(args.f)
+    pick_somas(args.f, args.d)
+
+
+
